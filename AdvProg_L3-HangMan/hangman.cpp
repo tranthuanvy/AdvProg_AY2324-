ecbb25f4b92ab522a7e98f398d3b1fb8b11608bf
unknown <thuanvytran4@gmail.com>
2024-03-14 20:50:21 +0700

    update hangman.cpp

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -70,10 +70,9 @@ bool isCharInWord(const char ch, const string& word)
 string chooseWordFromList(const vector<string>& wordList, int index) 
 {
     // TODO: Return a lowercase word in the index position of the vector wordList.
-    string answer;
-    answer=wordList[index];
+    
     if(index>=0&&index<wordList.size()){
-    	return answer;
+    	return wordList[index];
     }else{
     	return"";
     }
