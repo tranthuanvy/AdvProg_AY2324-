a21249ccbf8a13b26563469aaa59e47519aead8f
unknown <thuanvytran4@gmail.com>
2024-03-14 21:09:01 +0700

    update hangman.cpp

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -72,7 +72,11 @@ string chooseWordFromList(const vector<string>& wordList, int index)
     // TODO: Return a lowercase word in the index position of the vector wordList.
     
     if(index>=0&&index<wordList.size()){
-    	return wordList[index];
+    	string answer = wordList[index];
+    	for( char& c: answer ){
+    		c=tolower(c);
+    		
+    	}return answer;
     }else{
     	return"";
     }
