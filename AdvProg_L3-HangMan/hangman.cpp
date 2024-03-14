844764e7a0bac48af5bc562437fcc4598b989594
unknown <thuanvytran4@gmail.com>
2024-03-14 21:20:46 +0700

    update hangman.cpp

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -114,8 +114,11 @@ char getInputCharacter() {
 void updateSecretWord(string& secretWord, const char ch, const string& word)
 {
     // TODO: Update the secret word if the character ch is in the answer word.
-    for(int i=0;i<word.size();i++){
-    	if(word[i]==ch){
+     if (secretWord.size() != word.size()) {
+         return;
+    }
+	for(int i=0;i<word.size();i++){
+    	if(word[i]==ch&& secretWord[i] != ch){
     	secretWord[i]=ch;}
     }
 }
