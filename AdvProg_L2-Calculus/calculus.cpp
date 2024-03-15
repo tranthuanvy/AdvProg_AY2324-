ced17d91ae801330c78252c0c373703dff650ebb
unknown <thuanvytran4@gmail.com>
2024-03-15 16:12:16 +0700

    calculus hangman.cpp

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -19,9 +19,14 @@ double mySqrt(double x);
         double: cosine of x
 ***/
 double myCos(double x) 
-{  double cosx = 1-1/2*x*x+1/24*x*x*x*x-1/720*x*x*x*x*x*x;
-   
-    return cosx;
+{   int n=0;
+    double temp=1,result=0.0;
+    do{
+        result += temp;
+        n++;
+        temp *= -x*x/((2*n-1)*2*n);
+    }while( fabs(temp) >0.000001);
+    return result;
 }
 
 /***
@@ -31,8 +36,14 @@ double myCos(double x)
         double: sine of x
 ***/
 double mySin(double x)
-{ double  sinx=x-1/5*x*x*x+1/120*x*x*x*x*x-1/5040*x*x*x*x*x*x*x  ;  
-return sinx;
+{   double temp=x,result=0.0;
+    int n=0;
+    do {
+        result += temp;
+        n++;
+        temp *= -x*x/(2*n*(2*n+1));
+    }while (fabs (temp) > 0.000001);
+    return result;
 }
 
 
@@ -46,6 +57,15 @@ double mySqrt(double x) {
     if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
+    }
+    
+    else{
+        double temp, a=1.0;
+        while( fabs(temp*temp - x) > 0.000001 ){
+            temp = 0.5*(a+x/a);
+            a=temp;
+        }
+    return temp;
     }
 
     
